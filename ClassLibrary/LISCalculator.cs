602a4043a81e4824e0c284e687a6876d3bd29450
mshenth1 <mshenth@outlook.com>
2024-05-10 00:03:31 +1000

    Add project files.

---
+++ ClassLibrary/LISCalculator.cs
@@ -0,0 +1,40 @@
+﻿namespace LISCalculator
+{
+    public class LongestIncreasingSubsequence
+    {
+        public string FindLongestIncreasingSubsequence(string nums)
+        {
+            if (string.IsNullOrWhiteSpace(nums) || nums.Any(x => x != ' ' && !char.IsDigit(x)))
+            {
+                return "Invalid input!";
+            }
+
+            var arr = nums.Trim().Split(' ').Select(int.Parse).ToArray();
+
+            int currentSSLength = 1;
+            int longestSSLength = 1;
+            int startIndex = 0;
+            int endIndex = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    currentSSLength++;
+                    if (currentSSLength > longestSSLength)
+                    {
+                        longestSSLength = currentSSLength;
+                        startIndex = i + 2 - currentSSLength;
+                        endIndex = i + 2;
+                    }
+                }
+                else
+                {
+                    currentSSLength = 1;
+                }
+            }
+
+            return string.Join(" ", arr[startIndex..endIndex]);
+        }
+    }
+}
